6aae54eb25602ebf5fdcffa248515ecea5caaaca
Kuba <kubawozniak27@gmail.com>
2017-06-25 11:47:29 +0200

    Initial project

---
+++ Planer/Planer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Planer.Startup))]
+namespace Planer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
