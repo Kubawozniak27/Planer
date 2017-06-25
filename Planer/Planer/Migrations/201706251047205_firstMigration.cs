5ffa9560ba9ce62c10c302fb921771247b69477c
Kuba <kubawozniak27@gmail.com>
2017-06-25 12:50:57 +0200

    Edit account model

---
+++ Planer/Planer/Migrations/201706251047205_firstMigration.cs
@@ -0,0 +1,22 @@
+namespace Planer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class firstMigration : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "AccountName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+            DropColumn("dbo.AspNetUsers", "AccountName");
+        }
+    }
+}
---
+++ Planer/Planer/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Planer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Planer.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Planer.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(Planer.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
